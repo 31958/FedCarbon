418dd2c2b269c6c80fb607077c93a23b5e1c9222
Peixuan Song <60033220+31958@users.noreply.github.com>
2022-01-27 21:34:41 +0000

    Non iid data for clients simulation
    Using latent dirichlet allocation to give clients non iid data, and replicating the experiments in https://arxiv.org/pdf/2003.00295.pdf

---
+++ experiments_set_2/fedavg_lda.py
@@ -0,0 +1,92 @@
+import os
+import flwr.dataset.utils.common
+import time
+from multiprocessing import Process
+from typing import Tuple
+
+import flwr as fl
+import numpy as np
+import tensorflow as tf
+from flwr.server.strategy import FedAvg
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+
+def start_server(num_rounds: int, num_clients: int, fraction_fit: float):
+    """Start the server with a slightly adjusted FedAvg strategy."""
+    strategy = FedAvg(min_available_clients=num_clients, fraction_fit=fraction_fit)
+    # Exposes the server by default on port 8080
+    fl.server.start_server(strategy=strategy, config={"num_rounds": num_rounds},  server_address="localhost:8080")
+
+def start_client(dataset: Tuple[Tuple[np.ndarray, np.ndarray], Tuple[np.ndarray, np.ndarray]]) -> None:
+    """Start a single client with the provided dataset."""
+
+    # Load and compile a Keras model for CIFAR-10
+    model = tf.keras.applications.MobileNetV2((32, 32, 3), classes=10, weights=None)
+    model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
+
+    # Unpack the CIFAR-10 dataset partition
+    (x_train, y_train), (x_test, y_test) = dataset
+
+    # Define a Flower client
+    class CifarClient(fl.client.NumPyClient):
+        def get_parameters(self):
+            """Return current weights."""
+            return model.get_weights()
+
+        def fit(self, parameters, config):
+            """Fit model and return new weights as well as number of training
+            examples."""
+            model.set_weights(parameters)
+            # Remove steps_per_epoch if you want to train over the full dataset
+            # https://keras.io/api/models/model_training_apis/#fit-method
+            model.fit(x_train, y_train, epochs=1, batch_size=32, steps_per_epoch=3)
+            return model.get_weights(), len(x_train), {}
+
+        def evaluate(self, parameters, config):
+            """Evaluate using provided parameters."""
+            model.set_weights(parameters)
+            loss, accuracy = model.evaluate(x_test, y_test)
+            return loss, len(x_test), {"accuracy": accuracy}
+
+    # Start Flower client
+    fl.client.start_numpy_client("localhost:8080", client=CifarClient())
+
+def run_simulation(num_rounds: int, num_clients: int, fraction_fit: float):
+    """Start a FL simulation."""
+
+    # This will hold all the processes which we are going to create
+    processes = []
+
+    # Start the server
+    server_process = Process(
+        target=start_server, args=(num_rounds, num_clients, fraction_fit)
+    )
+    server_process.start()
+    processes.append(server_process)
+
+    # Optionally block the script here for a second or two so the server has time to start
+    time.sleep(2)
+
+    train, test = tf.keras.datasets.cifar10.load_data()
+
+    train = train[:num_clients * 100]
+    test = train[:num_clients * 50]
+
+    # Load the dataset partitions
+    train_partitions, pdf = flwr.dataset.utils.common.create_lda_partitions(dataset = train, num_partitions=num_clients, concentration=0.1)
+    test_partitions, pdf = flwr.dataset.utils.common.create_lda_partitions(dataset = test, num_partitions=num_clients,concentration=0.1)
+
+    # Start all the clients
+    for i in range (0, len(train_partitions)):
+        client_process = Process(target=start_client, args=((train_partitions[i],test_partitions[i]),))
+        client_process.start()
+        processes.append(client_process)
+
+    # Block until all processes are finished
+    for p in processes:
+        p.join()
+
+
+if __name__ == "__main__":
+    run_simulation(num_rounds=10, num_clients=10, fraction_fit=0.5)
